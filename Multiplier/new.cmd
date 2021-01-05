268d3770a3ba50f98bebabc62168407ddf58fec5
sairitwik27 <66405406+sairitwik27@users.noreply.github.com>
2021-01-05 19:11:37 +0530

    Add files via upload

---
+++ Multiplier/new.cmd
@@ -0,0 +1,63 @@
+stepsize 50
+h Vdd
+l Vss
+vector a a3 a2 a1 a0
+vector b b3 b2 b1 b0
+vector out r7 r6 r5 r4 r3 r2 r1 r0
+w a b out
+analyzer a b out
+setvector a [random -bitstream 4]
+setvector b [random -bitstream 4]
+s
+setvector a [random -bitstream 4]
+setvector b [random -bitstream 4]
+s
+setvector a [random -bitstream 4]
+setvector b [random -bitstream 4]
+s
+setvector a [random -bitstream 4]
+setvector b [random -bitstream 4]
+s
+setvector a [random -bitstream 4]
+setvector b [random -bitstream 4]
+s
+setvector a [random -bitstream 4]
+setvector b [random -bitstream 4]
+s
+setvector a [random -bitstream 4]
+setvector b [random -bitstream 4]
+s
+setvector a [random -bitstream 4]
+setvector b [random -bitstream 4]
+s
+setvector a [random -bitstream 4]
+setvector b [random -bitstream 4]
+s
+setvector a [random -bitstream 4]
+setvector b [random -bitstream 4]
+s
+setvector a [random -bitstream 4]
+setvector b [random -bitstream 4]
+s
+setvector a [random -bitstream 4]
+setvector b [random -bitstream 4]
+s
+setvector a [random -bitstream 4]
+setvector b [random -bitstream 4]
+s
+setvector a [random -bitstream 4]
+setvector b [random -bitstream 4]
+s
+setvector a [random -bitstream 4]
+setvector b [random -bitstream 4]
+s
+setvector a [random -bitstream 4]
+setvector b [random -bitstream 4]
+s
+setvector a [random -bitstream 4]
+setvector b [random -bitstream 4]
+s
+setvector a [random -bitstream 4]
+setvector b [random -bitstream 4]
+s
+powlogfile powerlog.log
---
+++ Multiplier/test2bit.cmd
@@ -0,0 +1,11 @@
+stepsize 50
+h Vdd
+l Vss
+vector a a1 a0
+vector b b1 b0
+vector out p3 p2 p1 p0
+w a b out
+analyzer a b out
+setvector a 11
+setvector b 11
+s
